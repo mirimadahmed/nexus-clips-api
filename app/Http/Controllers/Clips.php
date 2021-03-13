c029246ae904acf72382ca28cbdc711003d70a27
Mir Imad Ahmed <mirimadahmed@outlook.com>
2021-03-14 00:58:20 +0500

    sort

--- app/Http/Controllers/Clips.php
+++ app/Http/Controllers/Clips.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class Clips extends Controller
 {
     public function get_user_clips(Request $request) {
-        $clips = ModelsClips::where('user_id', '=', $request->user_id)->where('clip_status','=','ACTIVE')->paginate(10);
+        $clips = ModelsClips::where('user_id', '=', $request->user_id)->where('clip_status','=','ACTIVE')->orderBy('created_at', 'DESC')->paginate(10);
         return json_encode($clips);
     }
 }
