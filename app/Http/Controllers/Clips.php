a284461ada3104b5a622d6183c735157086df51a
Zeehshan <shanishah219@gmail.com>
2021-03-14 16:05:01 +0500

    user session

--- app/Http/Controllers/Clips.php
+++ app/Http/Controllers/Clips.php
@@ -3,12 +3,19 @@
 namespace App\Http\Controllers;
 
 use App\Models\Clips as ModelsClips;
+use App\Models\Session as SessionUser;
 use Illuminate\Http\Request;
 
 class Clips extends Controller
 {
     public function get_user_clips(Request $request) {
         $clips = ModelsClips::where('user_id', '=', $request->user_id)->where('clip_status','=','ACTIVE')->orderBy('created_at', 'DESC')->paginate(10);
+        if($request->page == 1){
+            $user = new SessionUser();
+            $user->user_id = $request->user_id;
+            $user->date = $request->Carbon::now();
+            $user->save();
+        }
         return json_encode($clips);
     }
 }
